aba8a905f9b25a569497ecb8be06716f3477059d
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-03-06 15:14:44 -0300

    bugfix(main server): corredc definitions

--- src/client/ConsoleChatListener.h
+++ src/client/ConsoleChatListener.h
@@ -17,19 +17,9 @@ private:
 public:
     ConsoleChatListener();
 
-    void onMessageReceived(Connection& conn, const ByteArray& data) override {
-        const std::unique_ptr<Message> msg = MessageFactory::create(data);
+    void onMessageReceived(Connection& conn, const ByteArray& data) override;
 
-        if (const auto it = printers.find(msg->getType()); it != printers.end()) {
-            it->second->handle(msg.get());
-        } else {
-            std::cerr << "[CLIENT]: Message of unknown type received\n";
-        }
-    }
-
-    void onDisconnected(Connection& conn) override {
-        std::cout << "[CLIENT] Disconnected from the server.\n";
-    }
+    void onDisconnected(Connection& conn) override;
 };
 
 

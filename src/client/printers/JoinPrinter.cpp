50fd59c79e60e37453f71ecb9b853161a637e42e
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-03-06 15:38:13 -0300

    style(printer): fixing error message

--- src/client/printers/JoinPrinter.cpp
+++ src/client/printers/JoinPrinter.cpp
@@ -12,7 +12,7 @@ void JoinPrinter::handle(Message *msg) {
     auto* joinMsg = dynamic_cast<JoinMessage*>(msg);
 
     if (!joinMsg)
-        throw std::invalid_argument("JoinMessage::handle received a non JoinMessage instance");
+        throw std::invalid_argument("JoinPrinter::handle received a non-JoinMessage instance");
 
     std::cout << ">>> " << joinMsg->getNickname() << " entrou no servidor! <<<\n";
 }

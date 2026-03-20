11c7a9014d535dafeeefdfbe8bdeef7fe9d7d92e
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-03-20 16:36:51 -0300

    feat(logger): implementing thread-safe class

---
+++ src/common/logger/LogLevel.h
@@ -0,0 +1,15 @@
+//
+// Created by Emilson Filho on 11/03/2026.
+//
+
+#ifndef CONEXAO98_LOGLEVEL_H
+#define CONEXAO98_LOGLEVEL_H
+
+enum class LogLevelEnum {
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+};
+
+#endif //CONEXAO98_LOGLEVEL_H
